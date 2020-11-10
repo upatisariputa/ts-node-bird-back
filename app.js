799ce27a6b2b57693e5a661fc85f911ce3fef686
upatisariputa <upatisariputa@gmail.com>
2020-11-10 14:12:04 +0900

    Make image upload route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const path = require("path");
 
 const userRouter = require("./routes/user");
 const postRouter = require("./routes/post");
@@ -33,6 +34,7 @@ app.use(
     credentials: true,
   })
 );
+app.use("/", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
--- routes/post.js
+++ routes/post.js
@@ -1,18 +1,50 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 const { Post, Comment, User, Image } = require("../models");
 const { isLoggedIn } = require("./middlewares");
 
 const router = express.Router();
 
-router.post("/", isLoggedIn, async (req, res, next) => {
-  console.log("포스트req post", req.body);
-  console.log("포스트req user", req.user);
+try {
+  fs.accessSync("uploads");
+} catch (e) {
+  console.log("There is not uploads foler");
+  fs.mkdirSync("uploads");
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "uploads");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      const basename = path.basename(file.originalname, ext);
+      done(null, basename + "_" + new Date().getTime() + ext);
+    },
+  }),
+  limits: { fileSize: 20 * 1024 * 1024 }, // 20MB
+});
+
+router.post("/", isLoggedIn, upload.none(), async (req, res, next) => {
   try {
     const post = await Post.create({
       content: req.body.content,
       UserId: req.user.id,
     });
+    if (req.body.image) {
+      if (Array.isArray(req.body.image)) {
+        const images = await Promise.all(req.body.image.map((image) => Image.create({ src: image })));
+        await post.addImages(images);
+      } else {
+        const image = await Image.create({ src: req.body.image });
+        await post.addImages(image);
+      }
+    }
+
     const fullPost = await Post.findOne({
       where: { id: post.id },
       include: [{ model: Image }, { model: Comment, include: [{ model: User, attributes: ["id", "nickname"] }] }, { model: User, attributes: ["id", "nickname"] }, { model: User, as: "Likers", attributes: ["id"] }],
@@ -88,6 +120,11 @@ router.delete("/:postId", isLoggedIn, async (req, res, next) => {
     console.error(e);
     next(e);
   }
+});
+
+router.post("/images", isLoggedIn, upload.array("image"), (req, res, next) => {
+  console.log(req.files);
+  res.json(req.files.map((v) => v.filename));
 });
 
 module.exports = router;
