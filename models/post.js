655207574dd8e9f107ac24042ed6f3b02957cbc9
upatisariputa <upatisariputa@gmail.com>
2020-11-10 14:45:31 +0900

    Make hashtag route and Change model

--- models/post.js
+++ models/post.js
@@ -1,27 +1,32 @@
-const { deepStrictEqual } = require("assert");
+const DataTypes = require("sequelize");
+const { Model } = DataTypes;
 
-module.exports = (sequelize, DataTypes) => {
-  const Post = sequelize.define(
-    "Post",
-    {
-      content: {
-        type: DataTypes.TEXT,
-        allowNull: false,
+module.exports = class Post extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        // id가 기본적으로 들어있다.
+        content: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+        // RetweetId
       },
-    },
-    {
-      charset: "utf8mb4",
-      collate: "utf8mb4_general_ci", //이모티콘 저장
-    }
-  );
-  Post.associate = (db) => {
-    db.Post.belongsTo(db.User);
-    db.Post.belongsToMany(db.Hashtag, { through: "PostHashtag" });
-    db.Post.hasMany(db.Comment);
-    db.Post.hasMany(db.Image);
-    db.Post.belongsTo(db.Post);
-    db.Post.belongsToMany(db.User, { through: "Like", as: "Likers" });
-    db.Post.belongsTo(db.Post, { as: "Retweet" });
-  };
-  return Post;
+      {
+        modelName: "Post",
+        tableName: "posts",
+        charset: "utf8mb4",
+        collate: "utf8mb4_general_ci", // 이모티콘 저장
+        sequelize,
+      }
+    );
+  }
+  static associate(db) {
+    db.Post.belongsTo(db.User); // post.addUser, post.getUser, post.setUser
+    db.Post.belongsToMany(db.Hashtag, { through: "PostHashtag" }); // post.addHashtags
+    db.Post.hasMany(db.Comment); // post.addComments, post.getComments
+    db.Post.hasMany(db.Image); // post.addImages, post.getImages
+    db.Post.belongsToMany(db.User, { through: "Like", as: "Likers" }); // post.addLikers, post.removeLikers
+    db.Post.belongsTo(db.Post, { as: "Retweet" }); // post.addRetweet
+  }
 };
